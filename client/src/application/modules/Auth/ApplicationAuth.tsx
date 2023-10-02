bc56afdb835b906384a469e95182cd8e6da0ab4f
Алексей Горулев <agorulev@vsegda-da.com>
2023-10-02 17:37:52 +0300

    asd

--- client/src/application/modules/Auth/ApplicationAuth.tsx
+++ client/src/application/modules/Auth/ApplicationAuth.tsx
@@ -34,7 +34,7 @@ export default function ApplicationAuth() {
     };
   }, [mounted]);
 
-  if (!mounted) return null;
+  if (!mounted || !!token) return null;
 
   return (
     <StyledAuthWrapper>
